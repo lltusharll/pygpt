63e544503ebd2772fb8201c047ba2175f11599cb
lltusharll <59608248+lltusharll@users.noreply.github.com>
2023-01-02 03:19:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import transformers
 
 app = Flask(__name__)
 
-# Load the GPT-2 model and tokenizer
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# Load the pretrained model and tokenizer
+model = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
 
-@app.route('/api', methods=['POST'])
-def generate_text():
-  # Get the input text from the request body
-  input_text = request.json['input']
-
-  # Encode the input text and generate text
-  input_ids = torch.tensor(tokenizer.encode(input_text)).unsqueeze(0)
-  output = model.generate(input_ids)
-  output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-  # Return the generated text as a response
-  return jsonify({ 'output': output_text })
+@app.route('/api', methods=['GET'])  # Allow the GET method
+def handle_request():
+    # Get the input text from the query string
+    input_text = request.args.get('input')
+    # Tokenize the input text and generate the response
+    input_ids = torch.tensor([tokenizer.encode(input_text, return_tensors='pt')])
+    output = model.generate(input_ids)
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    # Return the generated text as a JSON response
+    return {'output': output_text}
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
