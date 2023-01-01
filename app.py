2e1ffe0f710e77c16a81f1b89ed3952876378b3e
lltusharll <59608248+lltusharll@users.noreply.github.com>
2023-01-02 03:23:15 +0530

    tried to fix permission policy error

--- app.py
+++ app.py
@@ -8,16 +8,18 @@ app = Flask(__name__)
 model = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
 tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
 
-@app.route('/api', methods=['GET'])  # Allow the GET method
+@app.route('/api', methods=['POST'])
 def handle_request():
-    # Get the input text from the query string
-    input_text = request.args.get('input')
+    # Get the input text from the request body
+    input_text = request.json['input']
     # Tokenize the input text and generate the response
     input_ids = torch.tensor([tokenizer.encode(input_text, return_tensors='pt')])
     output = model.generate(input_ids)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    # Set the Permissions-Policy header to a supported value
+    headers = {'Permissions-Policy': 'geolocation=self'}
     # Return the generated text as a JSON response
-    return {'output': output_text}
+    return {'output': output_text}, 200, headers
 
 if __name__ == '__main__':
     app.run()
